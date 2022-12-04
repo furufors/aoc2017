55a79316367aafabf6e7dcc55dc876d9d58fdd43
Fredrik Furufors <furufors@gmail.com>
2022-12-04 15:26:49 +0100

    Added day 1

--- day01/day1p1.hs
+++ day01/day1p1.hs
@@ -1,4 +1,4 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map (\(a,b) -> if a==b then (read :: String -> Int) [a] else 0) . (\ss -> zip ss (tail ss)) . (\ss -> ss ++ [head ss]) . head . lines
