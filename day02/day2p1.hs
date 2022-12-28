da47d1e5b2e84d2f326527268999b90e7f530709
Fredrik Furufors <furufors@gmail.com>
2022-12-28 12:54:38 +0100

    Added day 2

--- day02/day2p1.hs
+++ day02/day2p1.hs
@@ -1,4 +1,20 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Either
+import Text.Parsec
+import Text.Parsec.Char
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum .  map (maxMinDiff . parseline) . lines
+
+maxMinDiff :: [Int] -> Int
+maxMinDiff is = let big = maximum is
+                    small = minimum is
+                in big - small
+
+parseline :: String -> [Int]
+parseline = fromRight [] . parse numbers ""
+    where
+        numbers = do
+            ns <- map read <$> many1 digit `sepBy` spaces
+            return ns
--- day02/day2p2.hs
+++ day02/day2p2.hs
@@ -1,4 +1,18 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Either
+import Text.Parsec
+import Text.Parsec.Char
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum .  map (divisible . parseline) . lines
+
+divisible :: [Int] -> Int
+divisible is = head [i `div` j | i <- is, j <- is, i /= j, i `mod` j == 0]
+
+parseline :: String -> [Int]
+parseline = fromRight [] . parse numbers ""
+    where
+        numbers = do
+            ns <- map read <$> many1 digit `sepBy` spaces
+            return ns
