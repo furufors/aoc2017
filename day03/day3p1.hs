0ae55edead5f43a44ce93106ab0952ba2a36bc6a
Fredrik Furufors <furufors@gmail.com>
2022-12-31 13:15:32 +0100

    Added day 4

--- day03/day3p1.hs
+++ day03/day3p1.hs
@@ -1,4 +1,9 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . solve . read
+
+solve :: Int -> Int
+solve pos = let sq = floor $ sqrt (fromIntegral pos)
+                circum = ((sq+1)*(sq+1) - sq*sq) `div` 4
+            in (sq-1) + abs (pos `mod` circum - (circum `div` 2stast))
--- day04/day4p1.hs
+++ day04/day4p1.hs
@@ -1,4 +1,9 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (nub)
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter valid . lines
+
+valid :: String -> Bool
+valid s = let ss = words s
+          in length ss == (length . nub $ ss)
