244782870a98bc37d8bf78330dca7a1bbf26f349
Fredrik Furufors <furufors@gmail.com>
2023-01-14 14:53:56 +0100

    Added day 5

--- day05/day5p1.hs
+++ day05/day5p1.hs
@@ -1,4 +1,26 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Debug.Trace
 main :: IO ()
-main = undefined
+main = interact $ show . runMaze . parse . lines
+
+parse :: [String] -> [Int]
+parse = map read
+
+runMaze :: [Int] -> Int
+runMaze jmps = runMaze' 0 0 jmps
+    where
+        runMaze' count ptr jmps | ptr < 0 = count
+        runMaze' count ptr jmps | ptr > (length jmps - 1) = count
+        runMaze' count ptr jmps =
+            let offset = jmps!!ptr
+                ptr' = ptr + offset
+                jmps' = increment ptr jmps
+                count' = count + 1
+            --in trace (show jmps ++ " : " ++ show ptr) $ runMaze' count' ptr' jmps'
+            in runMaze' count' ptr' jmps'
+
+increment :: Int -> [Int] -> [Int]
+increment _ [    ] = error "Reached end of list"
+increment 0 (i:is) = (i+1):is
+increment n (i:is) = i:(increment (n-1) is)
--- day05/day5p2.hs
+++ day05/day5p2.hs
@@ -1,4 +1,28 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Debug.Trace
 main :: IO ()
-main = undefined
+main = interact $ show . runMaze . parse . lines
+
+parse :: [String] -> [Int]
+parse = map read
+
+runMaze :: [Int] -> Int
+runMaze jmps = runMaze' 0 0 jmps
+    where
+        runMaze' count ptr jmps | ptr < 0 = count
+        runMaze' count ptr jmps | ptr > (length jmps - 1) = count
+        runMaze' count ptr jmps =
+            let offset = jmps!!ptr
+                ptr' = ptr + offset
+                jmps' = if offset >= 3
+                        then modify (\x -> x - 1) ptr jmps
+                        else modify (\x -> x + 1) ptr jmps
+                count' = count + 1
+            --in trace (show jmps ++ " : " ++ show ptr) $ runMaze' count' ptr' jmps'
+            in runMaze' count' ptr' jmps'
+
+modify :: (Int -> Int) -> Int -> [Int] -> [Int]
+modify f _ [    ] = error "Reached end of list"
+modify f 0 (i:is) = (f i):is
+modify f n (i:is) = i:(modify f (n-1) is)
