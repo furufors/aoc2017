1defc3dfda278d1626a90d666e42d5f1696eaff9
Fredrik Furufors <furufors@gmail.com>
2023-03-12 17:33:20 +0100

    Added day 7 part 1

--- day07/day7p1.hs
+++ day07/day7p1.hs
@@ -1,4 +1,8 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = putStrLn "hmvwl"
+
+-- No code necessary. Highlight any name in input file. Editor will highlight other occurances.
+-- After 5-7 steps there is just a single occurrance of the entry, this is the root.
+-- Solution: hmvwl
