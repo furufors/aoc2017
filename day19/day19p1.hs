da16b2f29e94c4e560c0f6737d9c695323104132
Fredrik Furufors <furufors@gmail.com>
2023-09-27 21:16:05 +0200

    Added day 19p1

--- day19/day19p1.hs
+++ day19/day19p1.hs
@@ -1,4 +1,84 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Char
+data Cell = Empty | Letter Char | Vert | Cross | Horiz deriving (Show, Eq)
+data Direction = W | N | E | S deriving (Eq)
+type Map = [[Cell]]
+
+data State = State
+    { xpos :: Int
+    , ypos :: Int
+    , dir :: Direction
+    , key :: String
+    }
+
 main :: IO ()
-main = undefined
+main = interact $ solve . map (map parseCell) . lines
+
+solve :: Map -> String
+solve m = runTubes startState (tile m)
+
+tile :: Map -> State -> Cell
+tile m s = if xok && yok
+           then m!!(ypos s)!!(xpos s)
+           else Empty
+    where yok = ypos s < (length m)
+          xok = xpos s < (length $ m!!(ypos s))
+
+-- s is current position
+runTubes :: State -> (State -> Cell) -> String
+runTubes s a = runTubes' s
+    where
+        runTubes' s = case a s of
+            Empty -> if key s == ""
+                     then runTubes' . right $ s
+                     else key s
+            Letter c -> runTubes' . continue . addLetter c $ s
+            Vert -> runTubes' . continue $ s
+            Horiz -> runTubes' . continue $ s
+            Cross -> let above = a (up s)
+                         leftOf = a (left s)
+                     in case dir s of
+                            W -> if cellIsLetter above || above == Vert
+                                 then runTubes' . continue $ s { dir = N}
+                                 else runTubes' . continue $ s { dir = S}
+                            N -> if cellIsLetter leftOf || leftOf == Horiz
+                                 then runTubes' . continue $ s { dir = W}
+                                 else runTubes' . continue $ s { dir = E}
+                            E -> if cellIsLetter above || above == Vert
+                                 then runTubes' . continue $ s { dir = N}
+                                 else runTubes' . continue $ s { dir = S}
+                            S -> if cellIsLetter leftOf || leftOf == Horiz
+                                 then runTubes' . continue $ s { dir = W}
+                                 else runTubes' . continue $ s { dir = E}
+
+cellIsLetter :: Cell -> Bool
+cellIsLetter (Letter _) = True
+cellIsLetter _          = False
+
+continue :: State -> State
+continue s = case dir s of
+    W -> left s
+    N -> up s
+    E -> right s
+    S -> down s
+
+right :: State -> State
+right s = let x' = xpos s + 1 in s { xpos = x'}
+left s  = let x' = xpos s - 1 in s { xpos = x'}
+up s    = let y' = ypos s - 1 in s { ypos = y'}
+down s  = let y' = ypos s + 1 in s { ypos = y'}
+
+addLetter :: Char -> State -> State
+addLetter c s = let key' = key s ++ [c] in s { key = key' }
+
+startState :: State
+startState = State 0 0 S ""
+
+parseCell :: Char -> Cell
+parseCell ' ' = Empty
+parseCell '|' = Vert
+parseCell '-' = Horiz
+parseCell '+' = Cross
+parseCell c | isLetter c = Letter c
+parseCell c = error $ "Error parsing: " ++ [c]
