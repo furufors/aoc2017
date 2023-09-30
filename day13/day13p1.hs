bae49da1c39dcffad8827b43faa50e423459fc86
Fredrik Furufors <furufors@gmail.com>
2023-09-30 18:27:15 +0200

    Added day 13

--- day13/day13p1.hs
+++ day13/day13p1.hs
@@ -1,4 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map (\(a,b) -> a * b) . filter scannerOverlap . map parse . lines
+
+parse :: String -> (Int, Int)
+parse s = let a = read . takeWhile (/= ':') $ s
+              b = read . tail . tail . dropWhile (/= ':') $ s
+          in (a, b)
+
+scannerOverlap :: (Int, Int) -> Bool
+scannerOverlap (a,b) = (a `mod` (2 * (b - 1))) == 0
--- day13/day13p2.hs
+++ day13/day13p2.hs
@@ -1,4 +1,19 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . findPerfectDelay . map parse . lines
+
+parse :: String -> (Int, Int)
+parse s = let a = read . takeWhile (/= ':') $ s
+              b = read . tail . tail . dropWhile (/= ':') $ s
+          in (a, b)
+
+findPerfectDelay :: [(Int, Int)] -> Int
+findPerfectDelay ps = testWithDelay 0
+    where
+        testWithDelay i = if null . filter (scannerOverlap i) $ ps
+                          then i
+                          else testWithDelay (i+1)
+
+scannerOverlap :: Int -> (Int, Int) -> Bool
+scannerOverlap d (a,b) = ((a + d) `mod` (2 * (b - 1))) == 0
